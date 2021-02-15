94dc2b2a17fe71d10848a0f05d407f17032ddf24
이기훈 <hunit612@gmail.com>
2021-02-15 17:00:38 +0900

    add login page

--- kh_django/kh_django/urls.py
+++ kh_django/kh_django/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from khuser.views import index, RegisterView
+from khuser.views import index, RegisterView, LoginView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
