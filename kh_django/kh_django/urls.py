9ebfe2c63bea18cf47393be4ca78e7ea25478932
이기훈 <hunit612@gmail.com>
2021-02-16 17:59:21 +0900

    create product detail page

--- kh_django/kh_django/urls.py
+++ kh_django/kh_django/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from khuser.views import index, RegisterView, LoginView
-from product.views import ProductList, ProductCreate
+from product.views import ProductList, ProductCreate, ProductDetail
 
 
 urlpatterns = [
@@ -25,5 +25,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
     path('product/', ProductList.as_view()),
+    path('product/<int:pk>/', ProductDetail.as_view()),
     path('product/create/', ProductCreate.as_view()),
 ]
