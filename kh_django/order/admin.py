31e1f78d0e69aaefd29bb118c657a80cdcbb61ee
이기훈 <hunit612@gmail.com>
2021-02-21 23:18:44 +0900

    add styled_status

--- kh_django/order/admin.py
+++ kh_django/order/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Order
+
+
 # Register your models here.
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('khuser', 'product', 'status')
+    list_filter = ('status',)
+    list_display = ('khuser', 'product', 'styled_status')
+
+    def styled_status(self, obj):
+        if obj.status == '환불':
+            return format_html(f'<span style="color:red">{obj.status}</span>')
+        if obj.status == '결제완료':
+            return format_html(f'<span style="color:green">{obj.status}</span>')
+        return obj.status
+
+    styled_status.short_description = '상태'
 
 admin.site.register(Order, OrderAdmin)
